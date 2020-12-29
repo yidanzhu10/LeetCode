b93c5112b7e37f1494063cb650e2043d539e5c0f
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2020-12-29 16:27:36 -0500

    Linked List Cycle

---
+++ src/udemy/L141_LinkedListCycle.java
@@ -0,0 +1,33 @@
+package udemy;
+
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class L141_LinkedListCycle {
+
+    // two pointers
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
