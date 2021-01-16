face60cea150fdbacb3d7f3cf20a1b9c52064080
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-01-16 12:35:50 -0500

    error corrected

--- src/udemy/L102_BinaryTreeLevelOrderTraversal.java
+++ src/udemy/L102_BinaryTreeLevelOrderTraversal.java
@@ -14,8 +14,9 @@ public class L102_BinaryTreeLevelOrderTraversal {
         Queue<TreeNode> q = new LinkedList();
         q.offer(root);
         while (!q.isEmpty()) {
+            int size = q.size();
             List<Integer> level = new ArrayList();
-            for (int i = 0; i < q.size(); i ++) {
+            for (int i = 0; i < size; i ++) {
                 TreeNode curr = q.poll();
                 level.add(curr.val);
                 if (curr.left != null) {
