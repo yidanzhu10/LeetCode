12a345d816e7c5c16c89aa84e258c11f3c0a9cf3
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-01-18 17:02:08 -0500

    Run-length encoded list

---
+++ src/L1313_DecompressRLEList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class L1313_DecompressRLEList {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> decompressList = new ArrayList();
+        int pair = nums.length / 2;
+        for(int i = 0; i < pair; i ++) {
+            int freq = nums[2 * i];
+            for (int j = 0; j < freq; j ++) {
+                decompressList.add(nums[2 * i + 1]);
+            }
+        }
+        int[] out = new int[decompressList.size()];
+        for(int i = 0; i < decompressList.size(); i ++) {
+            out[i] = decompressList.get(i);
+        }
+        return out;
+    }
+}
