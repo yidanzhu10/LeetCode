84b1186bf26a7f9146895191422bb6648f10f89e
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-01-18 20:37:05 -0500

    3ms

--- src/udemy/L19_RemoveNthNodeFromEndOfNode.java
+++ src/udemy/L19_RemoveNthNodeFromEndOfNode.java
@@ -21,10 +21,10 @@ public class L19_RemoveNthNodeFromEndOfNode {
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+//class ListNode {
+//    int val;
+//    ListNode next;
+//    ListNode() {}
+//    ListNode(int val) { this.val = val; }
+//    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//}
