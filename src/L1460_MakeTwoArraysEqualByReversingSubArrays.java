9a3a9713934c70aec11d41d6e87669291e1d6c9f
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-01-29 17:38:26 -0500

    Sort two arrays and check every single elements

---
+++ src/L1460_MakeTwoArraysEqualByReversingSubArrays.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class L1460_MakeTwoArraysEqualByReversingSubArrays {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length; i ++) {
+            if(target[i] != arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
