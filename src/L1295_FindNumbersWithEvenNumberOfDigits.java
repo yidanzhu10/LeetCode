86c8d312d895a93f720d3a3c3fbf17dcf9f63159
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-02-14 19:35:50 -0500

    Without converting to String

--- src/L1295_FindNumbersWithEvenNumberOfDigits.java
+++ src/L1295_FindNumbersWithEvenNumberOfDigits.java
@@ -8,4 +8,16 @@ public class L1295_FindNumbersWithEvenNumberOfDigits {
         }
         return count;
     }
+    public int findNumbers2(int[] nums) {
+        int count = 0;
+        for(int n : nums) {
+            int digit = 0;
+            while(n != 0) {
+                n = n / 10;
+                digit ++;
+            }
+            if(digit % 2 == 0) count ++;
+        }
+        return count;
+    }
 }
