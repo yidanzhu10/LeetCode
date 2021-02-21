4ca883b1afa5052dd5873756880eec04377fa3ce
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-02-20 19:06:59 -0500

    Line 9: check if there are two consecutive elements equal to val;

---
+++ src/L27_RemoveElement.java
@@ -0,0 +1,15 @@
+public class L27_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int len = nums.length;
+        for(int i = 0; i < len ; i ++) {
+            if(nums[i] == val) {
+                for(int j = i + 1; j < len; j ++) {
+                    nums[j - 1] = nums[j];
+                }
+                i --;
+                len --;
+            }
+        }
+        return len;
+    }
+}
