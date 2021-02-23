ac5d8f4cef5f452dec69b6e6bdfe3f9d0b6f7553
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-02-22 21:47:28 -0500

    Update element in place

--- src/L1299_ReplaceElementsWithGreatestElementOnRightSide.java
+++ src/L1299_ReplaceElementsWithGreatestElementOnRightSide.java
@@ -23,4 +23,17 @@ public class L1299_ReplaceElementsWithGreatestElementOnRightSide {
         }
         return arr;
     }
+    public int[] replaceElements3(int[] arr) {
+        if(arr.length == 1) arr[0] = -1;
+        int len = arr.length;
+        for(int i = 0; i < len - 1; i ++) {
+            int max = 0;
+            for(int j = i + 1; j < len; j ++) {
+                max = Math.max(max, arr[j]);
+            }
+            arr[i] = max;
+        }
+        arr[len - 1] = -1;
+        return arr;
+    }
 }
