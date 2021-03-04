721e907885dd3b2a5dbc192da0bfa58e5c316fa6
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-03-03 23:01:28 -0500

    take modulus of length for each elements

--- src/L448_FindAllNumbersDisappearedInAnArray.java
+++ src/L448_FindAllNumbersDisappearedInAnArray.java
@@ -9,7 +9,7 @@ public class L448_FindAllNumbersDisappearedInAnArray {
             nums[(nums[i] - 1) % len] += len;
         }
         for(int i = 0; i < len; i ++) {
-            if(nums[i] < len) list.add(i + 1);
+            if(nums[i] <= len) list.add(i + 1);
         }
         return list;
     }
