5e403b8628ab56025b31293530c0377f3d0ebaa9
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-03-18 22:49:51 -0400

    map.getOrDefault() finds the duplicate, edited the comments

--- src/L532_KdiffPairsInAnArray.java
+++ src/L532_KdiffPairsInAnArray.java
@@ -4,24 +4,18 @@ public class L532_KdiffPairsInAnArray {
     public int findPairs(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap();
         int res = 0;
-        //storing count of all the elements
+        //put all the elements in hashmap
         for(int i:nums){
             map.put(i, map.getOrDefault(i,0) + 1);
         }
-        for(int a : map.keySet()){
+        for(int a : map.keySet()){ // original array
             if(k != 0){
-                /*we avoid duplicate by looking for the values (i,j)
-                 where i is smaller element eg k=4 (1,3) & (3,1) gives same result
-                 but when we are processing 1 we are looking for val greater than 1 to form the pair
-                 and likewise when we are at 3 we are looking for val 3 and above to form the pair
-                 */
                 int b = a + k;
-                if(map.containsKey(b)){
+                if(map.containsKey(b)){ // avoid the duplicate
                     res ++;
                 }
             }else{
-                //processing the key so processing unique values which is occurring twice  and diff of it gives 0
-                if(map.get(a) >= 2)
+                if(map.get(a) >= 2) //if k ==0. there should be a duplicate
                     res ++;
             }
         }
