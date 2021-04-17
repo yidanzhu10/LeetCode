a4c3808968fe4659a25748b1b637b43074c6daa6
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-04-17 11:45:21 -0400

    Binary tree, iteration and recursion

---
+++ src/L104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+ public class L104_MaximumDepthOfBinaryTree {
+    // recursion
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+    public int maxDepth2(TreeNode root) {
+        if(root == null) return 0;
+        Stack<Integer> depth = new Stack();
+        Stack<TreeNode> stack = new Stack();
+        stack.add(root);
+        depth.add(1);
+
+        int height = 0;
+        while(!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            int currentHeight = depth.pop();
+            if(current != null){
+                height = Math.max(height, currentHeight);
+                stack.push(current.right);
+                depth.push(currentHeight + 1);
+                stack.push(current.left);
+                depth.push(currentHeight + 1);
+            }
+        }
+        return height;
+    }
+}
