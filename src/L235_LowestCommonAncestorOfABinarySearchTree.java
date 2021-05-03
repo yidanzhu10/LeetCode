7f9cfe338915fa9c0eb7e8b59624138ee0be85a1
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-05-03 13:35:57 -0400

    recursion

---
+++ src/L235_LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,12 @@
+public class L235_LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val < root.val && p.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
