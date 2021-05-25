efb94cbfd44bf6887b0940dada4a611c9aec7bd0
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-05-25 11:36:18 -0400

    let the slow pointer be at the point they will meet, and the fast point at head. Where they meet is the point the cycle starts.

---
+++ src/L0142_LinkedListCycleII.java
@@ -0,0 +1,29 @@
+public class L0142_LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) {
+                break;
+            }
+            // let the slow pointer be at the point they will meet
+        }
+
+        if(fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while(slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        /***
+        m + k * n + y = 2(m + y);
+        x + y = n; => m = (k - 1) n + x;
+         ***/
+
+        return slow;
+    }
+}
