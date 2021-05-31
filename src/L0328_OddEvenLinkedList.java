a8c365462988a95c3426392ee349ddac959fdb79
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-05-31 10:33:39 -0400

    two pointer

---
+++ src/L0328_OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+public class L0328_OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
