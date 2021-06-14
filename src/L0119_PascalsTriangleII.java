57b40166f43b51ecab06e85389d9954cfe8538a9
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-06-14 14:43:06 -0400

    similar to L118

---
+++ src/L0119_PascalsTriangleII.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class L0119_PascalsTriangleII {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList();
+        for(int i = 0; i <= rowIndex; i ++) {
+            list.add(0, 1);
+            for(int j = 1; j < list.size() - 1; j ++) {
+                list.set(j, list.get(j) + list.get(j + 1));
+            }
+        }
+        return list;
+    }
+}
