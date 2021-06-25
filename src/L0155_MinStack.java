f4300d355c0b71a1581951fb5e274130f78eafac
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-06-25 12:38:09 -0400

    Update min value and add two element into the stack when a new minimum value appears

---
+++ src/L0155_MinStack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class L0155_MinStack {
+}
+
+class MinStack {
+    Stack<Integer> stack;
+    int min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        this.stack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int val) {
+        if(val <= min) {
+            stack.push(min);
+            min = val;
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if(stack.pop() == min){
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
