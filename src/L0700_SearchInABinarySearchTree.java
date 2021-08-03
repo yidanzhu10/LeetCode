192328252b06f5f87c0faa17eca61773e0633914
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-08-02 20:23:13 -0400

    BST: smaller on the left, grater numbers on the right. base case needs to be separated into null and equal conditions

---
+++ src/L0700_SearchInABinarySearchTree.java
@@ -0,0 +1,11 @@
+public class L0700_SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null)return null;
+        if(root.val == val) return root;
+        if(root.val > val){
+            return searchBST(root.left, val);
+        } else{
+            return searchBST(root.right, val);
+        }
+    }
+}
