f171908b61d351101a1a90dde8d8b870f72ccfae
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-09-14 11:26:23 -0400

    recursion solution

--- src/L0021_MergeTwoSortedLists.java
+++ src/L0021_MergeTwoSortedLists.java
@@ -18,4 +18,15 @@ public class L0021_MergeTwoSortedLists {
         }
         return dummy.next;
     }
+    public ListNode mergeTwoLists_recursion(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val) {
+            l1.next = mergeTwoLists_recursion(l1.next, l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists_recursion(l1, l2.next);
+            return l2;
+        }
+    }
 }
