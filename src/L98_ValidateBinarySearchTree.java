1d173c3f3ff8e29a6ca0adda5f2aa47f40497d3f
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-09-20 22:45:18 -0400

    Long.MIN_VALUE and Long.MAX_VALUE for the corner case

--- src/L98_ValidateBinarySearchTree.java
+++ src/L98_ValidateBinarySearchTree.java
@@ -24,4 +24,13 @@ public class L98_ValidateBinarySearchTree {
 //        isValidBST(root);
         System.out.println(isValidBST(root));
     }
+    public boolean isValidBST2(TreeNode root){
+        if(root == null) return true;
+        return helper(root.left, Long.MIN_VALUE, root.val) && helper(root.right, root.val, Long.MAX_VALUE);
+    }
+    private boolean helper(TreeNode root, long min, long max){
+        if(root == null) return true;
+        if(root.val <= min || root.val >= max) return false;
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
 }
