2cbdbe12baeb106e2f5dc51fdbaf37a7c6f0fd32
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-09-29 22:50:13 -0400

    queen can go in vertical, horizontal and diagonal

--- src/L0052_NQueensII.java
+++ src/L0052_NQueensII.java
@@ -2,44 +2,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class L0052_NQueensII {
-    int res = 0;
+    int results = 0;
     public int totalNQueens(int n) {
-        if(n <= 0) return 0;
-        List<Integer> queenPos = new ArrayList();
+        if (n <= 0) {
+            return 0;
+        }
+
+        List<Integer> queenPos = new ArrayList<>();
         search(queenPos, n);
-        return res;
+        return results;
     }
+
     /*
-    results store all of the chessboards
-    queenPos stores the column Index or the position of queens for each row
+     * results store all of the chessboards
+     * queenPos store the column indices or the position of queens for each row
      */
-    private void search(List<Integer> queenPos, int n) {
+    private void search(List<Integer> queenPos,
+                        int n) {
+
         // there are already n queens in the list,
         // which means every row has been assigned a queen
-        if(queenPos.size() == n) {
-            res ++;
+        if (queenPos.size() == n) {
+            results++;
             return;
         }
-        for(int colIndex = 0; colIndex < n; colIndex ++){
-            if(isValid(queenPos, colIndex)){
+
+        for (int colIndex = 0; colIndex < n; colIndex++) {
+            if (!isValid(queenPos, colIndex)) {
                 continue;
             }
             queenPos.add(colIndex);
             search(queenPos, n);
-            // backtracking and explore if the queen can be placed into the next column
+            // backtracking, and explore if the queen can be placed into the next column
             queenPos.remove(queenPos.size() - 1);
         }
     }
-    private boolean isValid(List<Integer> queenPos, int col){
+
+    private boolean isValid(List<Integer> queenPos, int column) {
         int row = queenPos.size();
-        for(int rowIndex = 0; rowIndex < row; row ++){
+        for (int rowIndex = 0; rowIndex < row; rowIndex++) {
             int preQueenPos = queenPos.get(rowIndex);
-            if(preQueenPos == col) {
+            // same column, fail
+            if (preQueenPos == column) {
                 return false;
-            } // same column, fail
-            if((rowIndex + preQueenPos) == (row + col) || (rowIndex - preQueenPos) == (row - col)) {
+            }
+            // left diagonal or right diagonal, fail
+            if (rowIndex + preQueenPos == row + column
+                    || rowIndex - preQueenPos == row - column) {
                 return false;
-            } // left diagonal or right diagonal, fail
+            }
         }
         return true;
     }
