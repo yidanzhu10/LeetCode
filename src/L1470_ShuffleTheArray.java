74dff936e2d3c8259658b1d3c8e2bb6cb36fc22b
yidanzhu10 <50033529+yidanzhu10@users.noreply.github.com>
2021-11-02 22:06:50 -0400

    new method O(n) in time O(1) in space, the elements are shuffled in-place. 1024 is chosen because the maximum number in the array will be 1000. Choosing any integer greater than 1000 will be fine

--- src/L1470_ShuffleTheArray.java
+++ src/L1470_ShuffleTheArray.java
@@ -1,33 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class L1470_ShuffleTheArray {
     public int[] shuffle(int[] nums, int n) {
-        int[] list1 = new int[n];
-        int[] list2 = new int[n];
-        for (int i = 0; i < n; i ++) {
-            list1[i] = nums[i];
+        for(int i = n; i < nums.length; i ++) {
+            nums[i] = nums[i] * 1024 + nums[i - n];
+            // the maximum element in the array is 1000, choose any integer greater than 1000
         }
-        for(int j = 0; j <n; j ++) {
-            list2[j] = nums[j + n];
+        int index = 0;
+        for(int i = n; i < nums.length; i ++) {
+            nums[index] = nums[i] % 1024;
+            nums[index + 1] = nums[i] / 1024;
+            index += 2;
         }
-        List<Integer> list = new ArrayList<>();
-        for (int k = 0; k < n; k ++) {
-            list.add(list1[k]);
-            list.add(list2[k]);
-        }
-        int[] out = new int[nums.length];
-        for (int h = 0; h < list.size(); h ++) {
-            out[h] = list.get(h);
-        }
-        return out;
-    }
-    public int[] shuffle2(int[] nums, int n) {
-        int[] ans = new int[ 2 * n];
-        for (int i = 0; i < n; i ++) {
-            ans[2 * i] = nums[i];
-            ans[2 * i + 1] = nums[i + n];
-        }
-        return ans;
+        return nums;
     }
 }
